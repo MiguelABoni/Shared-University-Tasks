71ed922b5c29a11d3fe7a713d4733caeea8297af
MiguelABoni <miguelbedoyabonilla@gmail.com>
2022-12-12 18:38:05 -0500

    CRUD Ready

--- Database/Tasks.js
+++ Database/Tasks.js
@@ -17,6 +17,39 @@ class Tasks extends MongoDB {
             }
         });
     }
+
+    /** READ ALL */
+    async getTasks() {
+        return this.connect().then((db) => {
+            try {
+                return db.collection('Tasks').find().toArray();
+            } catch (err) {
+                return undefined;
+            }
+        });
+    }
+
+    /** UPDATE */
+    async updateTask(_id, data) {
+        return this.connect().then((db) => {
+            try {
+                return db.collection('Tasks').updateOne({ _id: ObjectId(_id) }, { $set: { ...data } });
+            } catch (err) {
+                return undefined;
+            }
+        });
+    }
+
+    /** DELETE */
+    async deleteTask(_id) {
+        return this.connect().then((db) => {
+            try {
+                return db.collection('Tasks').deleteOne({ _id: ObjectId(_id) });
+            } catch (err) {
+                return undefined;
+            }
+        });
+    }
 }
 
 module.exports = Tasks;
--- Routes/Tasks.controller.js
+++ Routes/Tasks.controller.js
@@ -14,4 +14,28 @@ TasksRouter.post('/new_task', async (req, res, next) => {
     next();
 })
 
+/** READ ALL TASKS */
+TasksRouter.get('/tasks', async (req, res, next) => {
+    let result = await TaskClient.getTasks();
+    req.result = result;
+    req.message = "INFO DE TODAS LAS TAREAS DEL CALENDARIO";
+    next();
+})
+
+/** UPDATE TASK */
+TasksRouter.put('/task/:id', async (req, res, next) => {
+    let result = await TaskClient.updateTask(req.params.id, req.body);
+    req.result = result;
+    req.message = "TAREA ACTUALIZADA CON ÉXITO";
+    next();
+})
+
+/** DELETE TASK */
+TasksRouter.delete('/task/:id', async (req, res, next) => {
+    let result = await TaskClient.deleteTask(req.params.id);
+    req.result = result;
+    req.message = "TAREA ELIMINADA CON ÉXITO";
+    next();
+})
+
 module.exports = TasksRouter;
